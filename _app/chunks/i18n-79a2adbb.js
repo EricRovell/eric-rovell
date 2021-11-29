a0820a05fa2fb20d0c8862adb4edd57fd8920ca8
EricRovell <32179779+EricRovell@users.noreply.github.com>
2021-11-29 10:53:17 +0000

    Deploying to gh-pages from @ EricRovell/ericrovell.me@8e168e60b0f88d12e59700d0a27fc544bb5eceeb 🚀

---
+++ _app/chunks/i18n-79a2adbb.js
@@ -0,0 +1,1 @@
+import{_ as a}from"./preload-helper-ec9aa979.js";import{W as r,Y as i,$ as _,Z as s,_ as n}from"./vendor-31f3363f.js";function d(e){i("en",()=>a(()=>import("./en-9186f39b.js"),[])),i("ru",()=>a(()=>import("./ru-f70d4bb9.js"),[])),_({fallbackLocale:e||"en",initialLocale:s()})}const f=r(n,(e,o)=>{o(e.slice(0,2))},"en");export{d as i,f as l};
